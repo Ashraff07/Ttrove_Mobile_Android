8abd47cab2f73ff2e57c2b01fe5c4e68df02835e
Ashraff Shaik <ashraff.x.shaik@kp.org>
2024-03-30 07:33:47 +0530

    Initial commit

---
+++ app/src/main/java/com/ttrove/mobile/android/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.ttrove.mobile.android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ttrove.mobile.android.ui.theme.Ttrove_Mobile_AndroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Ttrove_Mobile_AndroidTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Ttrove_Mobile_AndroidTheme {
+        Greeting("Android")
+    }
+}
